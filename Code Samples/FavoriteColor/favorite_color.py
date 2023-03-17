239a1c7e57e7a06a6fe395fd5b5c8055b4012b70
clara <10088152+claraj@users.noreply.github.com>
2023-03-16 20:11:40 -0500

    favorite color GET and POST requests

---
+++ Code Samples/FavoriteColor/favorite_color.py
@@ -0,0 +1,35 @@
+import requests
+
+colors_url = ''  # TODO we'll share the API URL in the workshop
+
+favorite_color = input('Please enter your favorite color: ')
+
+# Example JSON to send to API server
+json_data = {'color': favorite_color}
+add_color_response = requests.post(colors_url, json=json_data)
+add_color_json = add_color_response.json()
+
+# Optional, information for developers for debugging
+print(add_color_response.json())  # Hopefully this will be {"result":"Success - color submitted"}
+print(add_color_response.status_code)  # 201
+
+# Print a message for the user
+message = add_color_json.get('result')
+print(message)
+
+# TODO - error handling
+
+
+# Get all the favorite colors submitted so far
+
+all_colors_response = requests.get(colors_url)
+all_colors_json = all_colors_response.json()
+
+print(all_colors_json)  # For debugging, a list of objects
+# For example [{"name":"purple"},{"name":"orange"}]
+
+# Display the names of all the colors to the user
+for color in all_colors_json:
+    print(color['name'])
+
+# TODO error handling
